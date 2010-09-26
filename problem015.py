c7d7cc0858a6dd174d4b50ca08a2687b87d75be5
Paweł Nadolski <pawel.nadolski@gmail.com>
2010-09-26 20:26:41 +0200

    solved problem 15

--- problem015.py
+++ problem015.py
@@ -2,9 +2,17 @@
 edge = 20
 
 # solution 1
-sum = 0
-for i in range(edge):
-	sum += 2**(i)
-sum *= -2
-sum += 2**(edge+edge) - edge * 2
-print(sum)
+a = [[j for j in range(2, edge + 2)] for i in range(edge)]
+for i in range(1, edge):
+	for j in range(edge):
+		if j: a[i][j] = a[i-1][j] + a[i][j-1]
+		else: a[i][j] = a[i-1][j]+1
+solution = a[edge-1][edge-1]
+# solution 2
+f1, f2 = 1, 1
+for i in range(1, edge+1):
+	f1 *= i
+for i in range(edge+1, (2*edge)+1):
+	f2 *= i
+solution = f2//f1
+print(solution)
